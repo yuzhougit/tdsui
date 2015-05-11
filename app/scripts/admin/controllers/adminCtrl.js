91856d4a9e79aa78d3bb8583537d69832886563b
hi3hello3 <nuaazy@163.com>
2015-05-12 00:19:18 +0800

    complete admin of project

--- app/scripts/admin/controllers/adminCtrl.js
+++ app/scripts/admin/controllers/adminCtrl.js
@@ -18,7 +18,6 @@ define(['angular', '../../../constant'], function(angular,constant){
         	$scope.projects = data;
         });
 
-
         $scope.addProject = function(newProject) {
 
         	projects.post(newProject).then(function(data){
@@ -30,6 +29,47 @@ define(['angular', '../../../constant'], function(angular,constant){
 
         		$scope.projects.unshift(data);
         	});
+        }
+
+        $scope.updateProject = function(project) {
+
+            project.put({
+                product: project['product'],
+                release: project['release'],
+                platform: project['platform']
+            }).then(function(data){
+
+                if(data && data.errMsg) {
+                    $scope.addAlert('danger', data.errMsg);
+                    return;
+                }
+
+                for(var index=0; index < $scope.projects.length; index++) {
+                    if($scope.projects[index].id == project.id) {
+                        $scope.projects.splice(index,1,data);
+                        break;
+                    }
+                }
+
+                $scope.selectedProject = {};
+            });
+        }
+
+        $scope.editProject = function(project) {
+
+            $scope.selectedProject = Restangular.copy(project);
+        }
+
+        $scope.cancelEdit = function() {
+
+            $scope.selectedProject = {};
+        }
+
+        $scope.currentProject = null;
+        $scope.builds = {};
+        $scope.adminBuilds = function(project){
+
+            $scope.currentProject = project;
         }
 
 
